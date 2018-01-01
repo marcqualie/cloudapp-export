757879eba414bcbb75881e1285782d780cc600dd
Marc Qualie <marc@marcqualie.com>
2018-01-01 23:35:36 +0000

    Method in test should be renamed

--- test/cloudapp_export/exporter_test.rb
+++ test/cloudapp_export/exporter_test.rb
@@ -4,6 +4,6 @@ class CloudappExport::ExporterTest < Minitest::Test
   def test_it_can_initialize
     items = []
     exporter = ::CloudappExport::Exporter.new(items)
-    assert exporter.respond_to?(:export!)
+    assert exporter.respond_to?(:export_all)
   end
 end
