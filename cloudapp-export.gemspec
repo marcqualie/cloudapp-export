a921dc499747edb081e4fa2593ea1c7fb57f8b04
Marc Qualie <marc@marcqualie.com>
2019-10-26 14:17:16 +0100

    Use lcov formatter with latest cc-test-reporter

--- cloudapp-export.gemspec
+++ cloudapp-export.gemspec
@@ -28,4 +28,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "rubocop", "~> 0.75.1"
   spec.add_development_dependency "simplecov", "~> 0.17.1"
+  spec.add_development_dependency "simplecov-lcov", "> 0"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,18 @@
+# Code Coverage
 require "simplecov"
+require "simplecov-lcov"
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.output_directory = 'coverage' # default: "coverage/lcov"
+  c.lcov_file_name = 'lcov.info' # default: "YOUR_PROJECT_NAME.lcov"
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::LcovFormatter,
+  ],
+)
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
